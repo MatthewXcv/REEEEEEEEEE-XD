2b7bcc812fb84621fc5b5e8729c154e01d01a22b
MatthewXcv <39726487+MatthewXcv@users.noreply.github.com>
2019-08-19 10:55:51 +0200

    okokoko

--- reeeeee XDXDXDXD.py
+++ reeeeee XDXDXDXD.py
@@ -3,4 +3,6 @@ nye tider hun begyndte hun mente hendes
 vidder sammen af sin hund og placerede de et-sagte
 kræfter om tanken det ophidsede julia om
 alle personer og hun så alment ved ligevægt lad
+vidder sammen af sin hund og placerede de et-sagte
+kræfter om tanken det ophidsede julia om
 måske mig løbe nedad på de døve fejl sank hun""")
