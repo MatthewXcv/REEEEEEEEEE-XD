74186093935b5890da0131ff615dbcdef4e7fccf
Matiasborris <Matias_borris@live.dk>
2019-08-19 10:55:20 +0200

    Jeg har gjort mit

--- reeeeee XDXDXDXD.py
+++ reeeeee XDXDXDXD.py
@@ -1,6 +1,6 @@
 print("""Mellem diskret bisværm begynder hun at samle talerens
-nye tider hun begyndte hun mente hendes
+alle personer og hun så alment ved ligevægt lad
 vidder sammen af sin hund og placerede de et-sagte
 kræfter om tanken det ophidsede julia om
-alle personer og hun så alment ved ligevægt lad
+nye tider hun begyndte hun mente hendes
 måske mig løbe nedad på de døve fejl sank hun""")
