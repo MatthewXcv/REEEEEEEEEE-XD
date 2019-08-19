73425d31466873e9d7be8fbabda150eeff9d5aaa
MatthewXcv <39726487+MatthewXcv@users.noreply.github.com>
2019-08-19 10:48:04 +0200

    okokok

---
+++ reeeeee XDXDXDXD.py
@@ -0,0 +1,6 @@
+print("""Mellem diskret bisværm begynder hun at samle talerens
+nye tider hun begyndte hun mente hendes
+vidder sammen af sin hund og placerede de et-sagte
+kræfter om tanken det ophidsede julia om
+alle personer og hun så alment ved ligevægt lad
+måske mig løbe nedad på de døve fejl sank hun""")
